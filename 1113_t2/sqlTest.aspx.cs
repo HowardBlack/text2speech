ac310ae9fe40f3ae5ccee87b4c3d893c0b267c3e
星儒 林 <ace51718@gmail.com>
2023-03-17 14:07:57 +0800

    sql function OK

--- 1113_t2/sqlTest.aspx.cs
+++ 1113_t2/sqlTest.aspx.cs
@@ -22,7 +22,7 @@ namespace _1113_t2
         public static SqlConnection connection = null;
         public static SqlCommand command = null;
 
-        public static void sqlLink() { //資料庫連線
+        public static void SqlLink() { //資料庫連線
             string s_data =
             System.Web.Configuration.WebConfigurationManager.ConnectionStrings["Text2Speache"].ConnectionString;
             //從config找到資料庫位置[]內放的是Web.config的connectionStrings的name。
@@ -31,27 +31,31 @@ namespace _1113_t2
             connection.Open();//開啟通道 
         }
 
-        public static void sqlClose() { //關閉資料庫
+        public static void SqlClose() { //關閉資料庫
             connection.Close();
         }
 
-        public static List<List<string>> Reader() { //回傳reader裡的所有欄位資料
+        public static List<List<string>> Reader(string sql) { //回傳reader裡的所有欄位資料
+            Query(sql);
             SqlDataReader reader = command.ExecuteReader();
             List<List<string>> list = new List<List<string>>();
 
+            DataTable schemaTable = reader.GetSchemaTable();
+
             while (reader.Read()) {
                 List<string> temp = new List<string>();
-                for (int i = 0; i < reader.FieldCount; i++)
-                {
-                    temp.Add(reader[i].ToString());
-                }
 
-                list.Add(temp);
-                reader.NextResult();
+                foreach (DataRow row in schemaTable.Rows)
+                {
+                    string columnName = (string)row["ColumnName"];
+                    string columnValue = reader[columnName].ToString();
+                    temp.Add(columnValue);
+                    // 執行其他處理...
+                }
+                list.Add(temp);                
             }        
             
             reader.Close();
-
             return list;
         }
 
@@ -77,29 +81,28 @@ namespace _1113_t2
             return sql;
         }
 
-        public static List<List<string>> Get_Reader(string select, string from, string where) { //得到搜尋結果的陣列
-            Query(
-                Select(select, 
-                From(from), 
-                Where(where)
-               )
-            );
-
-            return Reader();
-        }
-
         [WebMethod]
         public static List<string> Get_Unicode(string pinyin) { //找到對應注音的所有文字的Unicode碼
             List<string> list = new List<string>(); //所查到的全部的字
 
-            List<List<string>> Pinyin = Get_Reader("id", "Pinyin", "pinyinText = N'" + pinyin + "'"); //找到PinyinId
+            List<List<string>> Pinyin = Reader(
+                Select("id", 
+                From("Pinyin"),
+                Where("pinyinText = N'" + pinyin + "'"))); //找到PinyinId
+
             foreach (var i in Pinyin) //i = Pinyin裡的欄位資料
             {
-                List<List<string>> Cns = Get_Reader("id", "Cns", "pinyinId = '" + i[0] + "'"); //找到CnsId
+                List<List<string>> Cns = Reader(
+                                            Select("id",
+                                            From("Cns"),
+                                            Where("pinyinId = '" + i[0] + "'")));//找到CnsId
 
                 foreach (var j in Cns) //j = Cns裡的欄位資料
                 {
-                    List<List<string>> Unicode = Get_Reader("code", "Unicode", "cnsId = '" + j[0] + "'"); //找到CnsId
+                    List<List<string>> Unicode = Reader(
+                                                    Select("code",
+                                                    From("Unicode"),
+                                                    Where("cnsId = '" + j[0] + "'"))); //找到Unicode
 
                     foreach (var k in Unicode)//k = Unicode裡的欄位資料
                     {
@@ -119,77 +122,9 @@ namespace _1113_t2
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            //讓資料庫寫入資料 
-            /*sqlLink();
-
-            SqlDataReader reader = null;
-            foreach (var a in getData())
-            {
-                string[] data = a.Split('\t');
-                //按順序解開註解寫完一個再重新註解一次後在執行下一個
-                Pinyin(data); //第一個
-                //Cns(data); //第二個
-                //Unicode(data); //第三個
-            }
-          
-            string[] getData() {
-                string file1 = System.IO.File.ReadAllText(
-                    //@"D:\\project\\eye0312\\eye\\1113_t2\\data\\CNS_phonetic.txt",
-                    @"D:\\project\\eye0312\\eye\\1113_t2\\data\\CNS2UNICODE_Unicode BMP.txt",
-                    Encoding.GetEncoding("utf-8")).Trim();
-
-                string[] f_array = file1.Split('\n');
-                return f_array;
-            }
-
-            void _Insert(string sql)
-            {
-                if (reader != null) reader.Close();
-                SqlCommand insert = new SqlCommand(sql, connection);
-                insert.ExecuteNonQuery();
-            }
-
-            void _Select(string sql)
-            {
-                if (reader != null) reader.Close();
-                SqlCommand select = new SqlCommand(sql, connection);
-                select.ExecuteNonQuery();
-                reader = select.ExecuteReader();
-            }
-
-            void Pinyin(string[] data)
-            {
-                _Select("select pinyinText from [Pinyin] where pinyinText = N'" + data[1].Trim() + "'");
-                if (reader.Read() == false)
-                {
-                    _Insert("insert into [Pinyin] values(N'" + data[1].Trim() + "')");
-                }
-            }
-
-            void Cns(string[] data)
-            {
-                _Insert(
-                    "insert into [Cns] values(" +
-                    "(select id from Pinyin where pinyinText = N'" + data[1].Trim() + "')," +
-                    "'" + data[0].Trim() + "'" +
-                    ")");
-            }
-
-            void Unicode(string[] data)
-            {
-                _Select("select id from Cns where code = '" + data[0].Trim() + "'");
-                
-                if (reader.Read() == true)
-                {
-                    _Insert(
-                    "insert into [Unicode] values("
-                    + reader["id"].ToString() + "," +
-                    "'" + data[1].Trim() + "'" +
-                    ")");
-                } 
-            }
-
-            sqlClose();*/
+            SqlLink();
+            List<string> test = Get_Unicode("ㄅㄨ");
+            SqlClose();
         }
     }
 }
