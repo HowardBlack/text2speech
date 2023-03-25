0cc29f621cd9a4d8f9bc968eb08b9fddb9d5a4f2
星儒 林 <ace51718@gmail.com>
2023-03-25 23:10:33 +0800

    api test

---
+++ 1113_t2/Controllers/PinyinController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Xml.Linq;
+
+namespace _1113_t2.Controllers
+{
+    public class PinyinController : ApiController
+    {
+        SqlConnection connection = new SqlConnection(
+            System.Web.Configuration.WebConfigurationManager.ConnectionStrings["Text2Speache"].ConnectionString;);
+        public static SqlCommand command = null;
+
+        public string Get() {
+            connection.Open();
+
+            connection.Close();
+            return "123";
+        }
+    }
+}
